4b8a8a88a75da170f3629b6fcb3dfedae156d1ec
tutajm <mtutaj@mcw.edu>
2023-07-13 11:55:45 -0500

    updated dependencies

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -5,7 +5,7 @@ APPDIR=/home/rgddata/pipelines/interaction-counts-pipeline
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 EMAIL_LIST=mtutaj@mcw.edu
 if [ "$SERVER" = "REED" ]; then
-  EMAIL_LIST=mtutaj@mcw.edu,jthota@mcw.edu,jdepons@mcw.edu
+  EMAIL_LIST=mtutaj@mcw.edu,jthota@mcw.edu
 fi
 
 $APPDIR/_run.sh "$@"
